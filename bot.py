298d425354bfab16b4321a7bbc27ff59e890c08b
kashif <firozkashif1234@gmail.com>
2020-06-20 02:49:54 +0530

    some changes

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
 import speech_recognition as sr 
+import pyttsx3
 
 r = sr.Recognizer()
 sample_rate = 48000
 chunk_size = 2048
 
+reply = {
+    "hi" : "Hi",
+    "hello" : "hello",
+    "hey" : "hey",
+    "your name" : "pybot",
+    "who" : "pybot"
+} #anytopic
 
 with sr.Microphone(sample_rate= sample_rate, chunk_size= chunk_size) as source:
     r.adjust_for_ambient_noise(source)
@@ -11,7 +19,16 @@ with sr.Microphone(sample_rate= sample_rate, chunk_size= chunk_size) as source:
     audio = r.listen(source)
 
 try:
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    for k,v in reply:
+        if k in r.recognize_google(audio).lower():
+            engine = pyttsx3.init()
+            engine.say(v)
+            engine.runAndWait()
+        else:
+            engine = pyttsx3.init()
+            engine.say("Sorry I couldn't understand that")
+            engine.runAndWait()
+
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
